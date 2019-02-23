2d2095c4ffb75bd28020c41e7f194c68d4db1a79
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2019-02-23 17:10:34 +0530

    Add Node.js support to application

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <cstdlib>
+#include <node.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_mobile_agentplatform_MainActivity_stringFromJNI(
@@ -7,4 +9,41 @@ Java_mobile_agentplatform_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_mobile_agentplatform_MainActivity_startNodeWithArguments(
+        JNIEnv *env,
+        jobject /* this */,
+        jobjectArray arguments) {
+
+    jsize argument_count = env->GetArrayLength(arguments);
+
+    int c_arguments_size = 0;
+    for (int i = 0; i < argument_count ; i++) {
+        c_arguments_size += strlen(env->GetStringUTFChars((jstring)env->GetObjectArrayElement(arguments, i), 0));
+        c_arguments_size++; // for '\0'
+    }
+
+    char* args_buffer = (char*) calloc(c_arguments_size, sizeof(char));
+
+    char* argv[argument_count];
+
+    char* current_args_position = args_buffer;
+
+    for (int i = 0; i < argument_count ; i++)
+    {
+        const char* current_argument = env->GetStringUTFChars((jstring)env->GetObjectArrayElement(arguments, i), 0);
+
+        strncpy(current_args_position, current_argument, strlen(current_argument));
+
+        argv[i] = current_args_position;
+
+        current_args_position += strlen(current_args_position) + 1;
+    }
+
+    int node_result = node::Start(argument_count, argv);
+    free(args_buffer);
+
+    return jint(node_result);
 }
--- app/src/main/java/mobile/agentplatform/MainActivity.kt
+++ app/src/main/java/mobile/agentplatform/MainActivity.kt
@@ -6,25 +6,35 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var _startedNodeAlready = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
         sample_text.text = stringFromJNI()
+
+        if (!_startedNodeAlready) {
+            _startedNodeAlready = true
+            Thread(Runnable {
+                startNodeWithArguments("node", "-e", "var http = require('http'); " +
+                        "var versions_server = http.createServer( (request, response) => { " +
+                        "  response.end('Versions: ' + JSON.stringify(process.versions)); " +
+                        "}); " +
+                        "versions_server.listen(3000);"
+                )
+            }).start()
+        }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     external fun stringFromJNI(): String
 
-    companion object {
+    external fun startNodeWithArguments(vararg argv:String): Integer
 
-        // Used to load the 'native-lib' library on application startup.
+    companion object {
         init {
             System.loadLibrary("native-lib")
+            System.loadLibrary("node")
         }
     }
 }
