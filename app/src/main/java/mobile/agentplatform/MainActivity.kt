18a59f51382a4aad6255eed61e2cd91ccc7787e3
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2019-06-13 11:22:18 +0530

    Request WRITE_EXTERNAL_STORAGE android permission

--- app/src/main/java/mobile/agentplatform/MainActivity.kt
+++ app/src/main/java/mobile/agentplatform/MainActivity.kt
@@ -1,12 +1,16 @@
 package mobile.agentplatform
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
 
     var fileContent: String? = null
     var projectPath: String? = null
@@ -14,12 +18,14 @@ class MainActivity : AppCompatActivity() {
     var nodeThread: Thread? = null
     var projectThread: Thread? = null
     var webViewUrl: String? = null
+    var PERMISSIONS_WRITE_EXTERNAL_STORAGE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         sample_text.text = stringFromJNI()
+        getPermission()
 
         fileSystem = FileSystem()
     }
@@ -97,6 +103,37 @@ class MainActivity : AppCompatActivity() {
         var temp = sample_text.text.toString()
         temp += "\n\n" + text
         sample_text.text = temp
+    }
+
+    fun getPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSIONS_WRITE_EXTERNAL_STORAGE
+            )
+        } else {
+            printLog("LOG: (getPermission) PERMISSION_GRANTED")
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSIONS_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                printLog("LOG: (onRequestPermissionsResult) PERMISSION_GRANTED")
+            } else {
+                printLog("LOG: (onRequestPermissionsResult) PERMISSION_DENIED")
+            }
+        }
     }
 
     external fun stringFromJNI(): String
